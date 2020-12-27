4a28bd7ee72c81922f760892fd391167bcc1f473
martinc <chung.martin7788@gmail.com>
2020-12-27 16:51:29 -0500

    added

---
+++ swap.c
@@ -0,0 +1,19 @@
+#include <studio.h>
+
+int main(void)
+{
+    int x = 34;
+    int y = 50;
+
+    printf("x is %i\n", x);
+    printf("y is %i\n", y);
+
+    // swapping ...
+    int temp = x;
+    x = y;
+    y = temp;
+
+    printf("x is now %i\n", x);
+    printf("y is now %i\n", y);
+
+}

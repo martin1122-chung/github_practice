93901dc64766cacc4ad8afbe6b23c9021a350ff1
martinc <chung.martin7788@gmail.com>
2020-12-29 17:58:21 -0500

    added test1.c

---
+++ test1.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+int main(void)
+{
+  int x = 34;
+  int y = 50;
+
+  printf("x is %i\n", x);
+  printf("y is %i\n", y);
+
+  if (x >= 10)
+  {
+    printf("x is greater than 10.")
+  }
+}
